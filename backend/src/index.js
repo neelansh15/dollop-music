116d8b21917c531e46ef0f40c9bab14bbb7de73a
VedantMahadik <vedant@Legion-Y540.localdomain>
2021-11-03 21:07:13 +0530

    ft:Add details route for profile
    Signed-off-by: VedantMahadik <vedant@Legion-Y540.localdomain>

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+// const express = require("express");
+// const app = express();
+// const router = express.Router();
+
+// app.use(express.json());
+
+// router.use("/profile", require("./profile"));
+
+// app.listen(port, () => {
+//   console.log(`listening on ${port}...`);
+// });
+
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+// anything beginning with "/api" will go into this
+app.use("/api", require("./routes/api"));
+
+app.listen(port, () => {
+  console.log("Listening on", port);
+});
---
+++ backend/src/routes/api/profile.js
@@ -0,0 +1,22 @@
+var router = require("express").Router();
+const client = require("../../db");
+
+router.post("/details", (req, res) => {
+  const body = req.body;
+  const obj = {
+    name: body.name,
+    about: body.about,
+  };
+
+  client.connect(async (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const collection = client.db("Cluster0").collection("users");
+    await collection.insertOne(obj);
+    client.close();
+  });
+});
+
+module.exports = router;
