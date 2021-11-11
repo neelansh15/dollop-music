54466269239ff8fdba387b4d015e1c2dbb305914
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2021-11-11 22:13:38 +0530

    bug: Profile page not rendered
    Profile page not rendering :cry:

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,17 +1,5 @@
 import "windi.css";
-import Head from "next/head";
 
 export default function Home() {
-  return (
-    <div className='container'>
-      <Head>
-        <title>Dollop Music</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <main>
-        <h1 className='text-red-500 text-9xl'>Home</h1>
-      </main>
-    </div>
-  );
+  return <main></main>;
 }
