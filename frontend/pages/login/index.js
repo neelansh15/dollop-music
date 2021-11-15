d4ead147d3124b494900230827ce8a7aadff182e
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-15 22:24:25 +0530

    ft: updat ewindi colors

--- frontend/pages/login/index.js
+++
@@ -1,5 +0,0 @@
-export default function Login() {
-  return (
-    <h1 className="text-blue-500 text-9xl">Login</h1>
-  );
-}
---
+++ frontend/pages/login/index.jsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <h1 className="text-blue-500 text-9xl">Login</h1>;
+}
