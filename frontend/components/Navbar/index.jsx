36b06996af000885e7ab62ad4758447c57d17ee1
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-19 22:08:36 +0530

    ft: navbar component

---
+++ frontend/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+function Navbar(){
+    return(
+        <div className="py-5 px-10 w-full flex justify-between items-center bg-dark-500">
+            <h1>Dollop Music</h1>
+            <ul className="list-none flex space-x-4 items-center text-sm">
+                <li>Dashboard</li>
+                <li>Search</li>
+                <li>Profile</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Navbar
--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,8 +1,10 @@
 import "windi.css";
+import Navbar from "../components/Navbar";
 
 export default function Home({ Component }) {
   return (
-    <div className="h-screen w-full">
+    <div className="h-screen w-full bg-dark-800 text-white">
+      <Navbar />
       <main>
         <Component />
       </main>
