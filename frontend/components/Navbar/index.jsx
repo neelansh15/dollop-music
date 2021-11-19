fa62b7d8186cb7383aa31820f3c028d212363304
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-19 22:34:05 +0530

    ft: navbar

--- frontend/components/Navbar/index.jsx
+++ frontend/components/Navbar/index.jsx
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 function Navbar(){
     return(
         <div className="py-5 px-15 w-full flex justify-between items-center bg-dark-500">
             <h1>Dollop Music</h1>
             <ul className="list-none flex space-x-4 items-center text-sm">
-                <li>Dashboard</li>
-                <li>Search</li>
-                <li>Profile</li>
+                <Link href="/"><li className="cursor-pointer">Dashboard</li></Link>
+                <Link href="/search"><li className="cursor-pointer">Search</li></Link>
+                <Link href="/profile"><li className="cursor-pointer">Profile</li></Link>
             </ul>
         </div>
     )
