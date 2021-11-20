fa8081e51b1c21b4bc0f79beba78fd704705ce5d
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2021-11-20 20:33:05 +0530

    ft: Add remove from following route in API

--- backend/functions/routes/api/follow.js
+++ backend/functions/routes/api/follow.js
@@ -17,13 +17,6 @@ router.post("/addFollowing", (req, res) => {
     }
     const collection = client.db("Dollop").collection("users");
 
-    collection.find({ _id: userId }).toArray((err, res) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(res);
-    });
     await collection.updateOne(
       { _id: userId },
       { $push: { following: followeeId } },
@@ -34,7 +27,35 @@ router.post("/addFollowing", (req, res) => {
     );
     client.close();
   });
-  res.status(200).send("Added follower");
+  res.status(200).send("Added to following");
+});
+
+router.post("/removeFollowing", (req, res) => {
+  const body = req.body;
+  var userId = new ObjectId(req.body.userId);
+
+  var followeeId = new ObjectId(req.body.followeeId);
+  console.log(userId, followeeId);
+
+  client.connect(async (err, res) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send("err");
+      return;
+    }
+    const collection = client.db("Dollop").collection("users");
+
+    await collection.updateOne(
+      { _id: userId },
+      { $pull: { following: followeeId } },
+    );
+    await collection.updateOne(
+      { _id: followeeId },
+      { $pull: { followers: userId } },
+    );
+    client.close();
+  });
+  res.status(200).send("Removed from following");
 });
 
 module.exports = router;
