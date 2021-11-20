8d9cc3bf01da97e572fa7942bd9bc1be27b93785
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-21 00:10:37 +0530

    ft: test data for music listing

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -2,6 +2,24 @@ import { Card } from "./Card";
 import MusicItem from "./MusicItem";
 
 export default function Home() {
+  const popularMusic = [
+    {
+      name: "Music Name 1",
+      artists: ["Neelansh", "Vedant"],
+      clapCount: 1000,
+    },
+    {
+      name: "Music Name 2",
+      artists: ["KSHMR"],
+      clapCount: 2400,
+    },
+    {
+      name: "Music Name 3",
+      artists: ["Lil Nas X", "Drake"],
+      clapCount: 579,
+    },
+  ];
+
   return (
     <div className="px-15 pt-10 w-full">
       <h1 className="text-3xl font-bold">Dashboard</h1>
@@ -11,8 +29,10 @@ export default function Home() {
         <div className="col-span-5 md:col-span-3">
           <Card>
             <h1 className="text-xl font-semibold mb-5">Popular Songs</h1>
-            
-            <MusicItem />
+
+            {popularMusic.map((music) => (
+              <MusicItem music={music} />
+            ))}
           </Card>
         </div>
         <div className="col-span-5 md:col-span-2">
