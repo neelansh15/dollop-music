b1b5a59b5bd14aef5912f528b7ee92caa312f48f
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2021-12-01 23:22:51 +0530

    ft: Add send token functionality on login and register

--- backend/functions/routes/api/music.js
+++ backend/functions/routes/api/music.js
@@ -15,117 +15,132 @@ Routes:
 */
 
 router.get("/most_clapped", (req, res) => {
-  const body = req.body;
-  client.connect(async (err, res) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send("err");
+  try {
+    const body = req.body;
+    client.connect(async (err, res) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+      const collection = client.db("Dollop").collection("music");
+      await collection
+        .find({})
+        .sort({ claps: -1 })
+        .toArray((err, data) => {
+          if (err) {
+            res.status(400).send("Error in finding");
+            return;
+          }
+          res.status(200).send(data);
+        });
+      client.close();
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+router.post("/", upload.array("uploadedFile", 5), (req, res) => {
+  try {
+    const body = req.body;
+    var image, music;
+    for (i in req.files) {
+      if (req.files[i].mimetype.split("/")[0] === "image") {
+        image = req.files[i];
+      } else if (req.files[i].mimetype.split("/")[0] === "audio") {
+        music = req.files[i];
+      }
+    }
+    const obj = {
+      userId: body.userId ? body.userId : -1,
+      albumId: body.albumId ? body.albumId : -1,
+      name: body.name ? body.name : "Music",
+      genre: body.genre ? body.genre : "None",
+      image: body.image ? body.image : "https://bit.ly/2Z7Plfp",
+      date: body.date ? body.date : Date.now(),
+      music: body.music,
+      claps: 0,
+    };
+    console.log(obj.userId);
+    if (obj.userId == -1) {
+      res.status(400).send("No userId provided");
       return;
     }
-    const collection = client.db("Dollop").collection("music");
-    await collection
-      .find({})
-      .sort({ claps: -1 })
-      .toArray((err, data) => {
+    var userId = req.body.userId;
+    client.connect(async (err, res) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+
+      var mimetype = image.mimetype;
+      mimetype = mimetype.split("/");
+      var extension = mimetype[1];
+      console.log("Extension", extension);
+      console.log(image.buffer);
+      const file = bucket.file(`Images/${obj.userId}/${obj.name}.${extension}`);
+      await file.save(image.buffer, { contentType: image.mimetype });
+      file.makePublic();
+      const imgLink = file.publicUrl();
+
+      mimetype = music.mimetype.split("/");
+      extension = mimetype[1];
+      console.log("Extension", extension);
+      const musicFile = bucket.file(
+        `Music/${obj.userId}/${obj.name}.${extension}`,
+      );
+      await musicFile.save(music.buffer, { contentType: music.mimetype });
+      musicFile.makePublic();
+      const musicLink = musicFile.publicUrl();
+
+      obj.image = imgLink;
+      obj.music = musicLink;
+      const musicCollection = client.db("Dollop").collection("music");
+      let musicId = await musicCollection.insertOne(obj);
+
+      const userCollection = client.db("Dollop").collection("users");
+      await userCollection.updateOne(
+        { _id: userId },
+        { $push: { music: musicId.insertedId } },
+      );
+      client.close();
+    });
+    res.status(200).send("Added music to db");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+router.get("/", (req, res) => {
+  try {
+    const body = req.body;
+
+    const musicId = body.ids.map(x => new ObjectId(x));
+    client.connect(async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+      const collection = client.db("Dollop").collection("music");
+
+      await collection.find({ _id: { $in: musicId } }).toArray((err, data) => {
         if (err) {
           res.status(400).send("Error in finding");
           return;
         }
         res.status(200).send(data);
       });
-    client.close();
-  });
-});
-
-router.post("/", upload.array("uploadedFile", 5), (req, res) => {
-  const body = req.body;
-  var image, music;
-  for (i in req.files) {
-    if (req.files[i].mimetype.split("/")[0] === "image") {
-      image = req.files[i];
-    } else if (req.files[i].mimetype.split("/")[0] === "audio") {
-      music = req.files[i];
-    }
-  }
-  const obj = {
-    userId: body.userId ? body.userId : -1,
-    albumId: body.albumId ? body.albumId : -1,
-    name: body.name ? body.name : "Music",
-    genre: body.genre ? body.genre : "None",
-    image: body.image ? body.image : "https://bit.ly/2Z7Plfp",
-    date: body.date ? body.date : Date.now(),
-    music: body.music,
-    claps: 0,
-  };
-  console.log(obj.userId);
-  if (obj.userId == -1) {
-    res.status(400).send("No userId provided");
-    return;
-  }
-  var userId = req.body.userId;
-  client.connect(async (err, res) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send("err");
-      return;
-    }
-
-    var mimetype = image.mimetype;
-    mimetype = mimetype.split("/");
-    var extension = mimetype[1];
-    console.log("Extension", extension);
-    console.log(image.buffer);
-    const file = bucket.file(`Images/${obj.userId}/${obj.name}.${extension}`);
-    await file.save(image.buffer, { contentType: image.mimetype });
-    file.makePublic();
-    const imgLink = file.publicUrl();
-
-    mimetype = music.mimetype.split("/");
-    extension = mimetype[1];
-    console.log("Extension", extension);
-    const musicFile = bucket.file(
-      `Music/${obj.userId}/${obj.name}.${extension}`,
-    );
-    await musicFile.save(music.buffer, { contentType: music.mimetype });
-    musicFile.makePublic();
-    const musicLink = musicFile.publicUrl();
-
-    obj.image = imgLink;
-    obj.music = musicLink;
-    const musicCollection = client.db("Dollop").collection("music");
-    let musicId = await musicCollection.insertOne(obj);
-
-    const userCollection = client.db("Dollop").collection("users");
-    await userCollection.updateOne(
-      { _id: userId },
-      { $push: { music: musicId.insertedId } },
-    );
-    client.close();
-  });
-  res.status(200).send("Added music to db");
-});
-
-router.get("/", (req, res) => {
-  const body = req.body;
-
-  const musicId = body.ids.map(x => new ObjectId(x));
-  client.connect(async (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send("err");
-      return;
-    }
-    const collection = client.db("Dollop").collection("music");
-
-    await collection.find({ _id: { $in: musicId } }).toArray((err, data) => {
-      if (err) {
-        res.status(400).send("Error in finding");
-        return;
-      }
-      res.status(200).send(data);
+      client.close();
     });
-    client.close();
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
--- backend/functions/routes/api/profile.js
+++ backend/functions/routes/api/profile.js
@@ -1,199 +1,268 @@
 var router = require("express").Router();
 const { client, firebaseApp, bucket } = require("../../db");
-var ObjectId = require("mongodb").ObjectId;
 const multer = require("multer");
 let upload = multer({ storage: multer.memoryStorage() });
-const { signup, login } = require("./users");
 var crypto = require("crypto");
 
-router.post("/update", upload.array("uploadedFile", 5), async (req, res) => {
-  const body = req.body;
-  const obj = body.obj;
-  client.connect(async (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send("err");
-      return;
-    }
-
-    if (req.file) {
-      var image = req.files[0];
-      var bannerImage = req.files[0];
-
-      var mimetype = image.mimetype;
-      mimetype = mimetype.split("/");
-      var extension = mimetype[1];
-      console.log(image.buffer);
-      const file = bucket.file(`Images/${obj._id}/${obj.name}.${extension}`);
-      await file.save(image.buffer, { contentType: image.mimetype });
-      file.makePublic();
-      const imgLink = file.publicUrl();
-      obj.image = imgLink;
-
-      mimetype = bannerImage.mimetype;
-      mimetype = mimetype.split("/");
-      extension = mimetype[1];
-      console.log(bannerImage.buffer);
-      const bannerImageFile = bucket.file(
-        `Images/${obj._id}/${obj.name}.${extension}`,
-      );
-      await bannerImageFile.save(bannerImage.buffer, {
-        contentType: bannerImage.mimetype,
-      });
-      bannerImageFile.makePublic();
-      const bannerImageLink = bannerImageFile.publicUrl();
-      obj.bannerImage = bannerImageLink;
-    }
-
-    const collection = client.db("Dollop").collection("users");
-    await collection.updateOne({ _id: body.id }, { $set: { obj } });
-    client.close();
-  });
-  res.status(201).send("Success");
-});
-
-router.get("/:id", (req, res) => {
-  const body = req.body;
-  const params = req.params;
-  const docId = params.id;
-  client.connect(async (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send("err");
-      return;
-    }
-    const collection = client.db("Dollop").collection("users");
-    collection.find({ _id: docId }).toArray((err, result) => {
+router.get("/", (req, res) => {
+  // body has email id as id
+  try {
+    const body = req.body;
+    const docId = body.id;
+    client.connect(async (err, data) => {
       if (err) {
         console.log(err);
         res.status(400).send("err");
         return;
       }
-      res.status(200).send(result);
-      client.close();
+      const collection = client.db("Dollop").collection("users");
+      collection.find({ _id: docId }).toArray((err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(400).send("err");
+          return;
+        }
+        res.status(200).send(result);
+        client.close();
+      });
     });
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 router.post("/register", (req, res) => {
-  const body = req.body;
-  body.password = crypto.createHash("md5").update(body.password).digest("hex");
-  const obj = {
-    _id: body.email,
-    password: body.password,
-    name: "User",
-    username: body.username,
-    tagline: "No tags",
-    about: "No about",
-    instagram: "#",
-    soundcloud: "#",
-    twitter: "#",
-    github: "#",
-    image: "https://bit.ly/3HCahMK",
-    bannerImage: "https://bit.ly/3x2aiVA",
-    activeSession: "",
-    followers: [],
-    following: [],
-    music: [],
-  };
-  client.connect(async (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send("err");
-      return;
-    }
-    const collection = client.db("Dollop").collection("users");
-    await collection.insertOne(obj);
-    collection.updateOne(
-      { _id: body.email },
-      { $set: { activeSession: hash } },
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          res.status(400).send("err while adding session");
+  // body has email pass and username
+  try {
+    const body = req.body;
+    const hash = crypto.createHash("md5").digest("hex");
+    body.password = crypto
+      .createHash("md5")
+      .update(body.password)
+      .digest("hex");
+    const obj = {
+      _id: body.email,
+      password: body.password,
+      name: "User",
+      username: body.username,
+      tagline: "No tags",
+      about: "No about",
+      instagram: "#",
+      soundcloud: "#",
+      twitter: "#",
+      github: "#",
+      image: "https://bit.ly/3HCahMK",
+      bannerImage: "https://bit.ly/3x2aiVA",
+      activeSession: "",
+      followers: [],
+      following: [],
+      music: [],
+    };
+    client.connect(async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+      const collection = client.db("Dollop").collection("users");
+      await collection.insertOne(obj);
+      const token = crypto.randomBytes(20).toString("hex");
+      collection.updateOne(
+        { _id: body.email },
+        { $set: { activeSession: token } },
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            res.status(400).send("err while adding session");
+            client.close();
+            return;
+          }
+          res.status(200).send(token);
           client.close();
-          return;
-        }
-        res.status(200).send("Login success");
-      },
-    );
-    client.close();
-  });
+        },
+      );
+      client.close();
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 router.post("/login", (req, res) => {
+  // body has email and pass
+  try {
+    const body = req.body;
+
+    client.connect(async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+      const collection = client.db("Dollop").collection("users");
+      await collection
+        .find({ _id: body.email })
+        .project({ password: 1 })
+        .toArray((err, result) => {
+          if (err) {
+            console.log(err);
+            client.close();
+            res.status(400).send("err");
+            return;
+          }
+          if (
+            crypto.createHash("md5").update(body.password).digest("hex") ==
+            result[0].password
+          ) {
+            console.log("pass");
+            const token = crypto.randomBytes(20).toString("hex");
+            collection.updateOne(
+              { _id: body.email },
+              { $set: { activeSession: token } },
+              (err, result) => {
+                if (err) {
+                  console.log(err);
+                  res.status(400).send("err while adding session");
+                  client.close();
+                  return;
+                }
+                res.status(200).send(token);
+                client.close();
+              },
+            );
+          } else {
+            console.log(body.password, result[0].password);
+            client.close();
+            res.status(400).send("Invalid credentials");
+          }
+        });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  // body has email
+  try {
+    const body = req.body;
+    client.connect(async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+      const collection = client.db("Dollop").collection("users");
+      collection.updateOne(
+        { _id: body.email },
+        { $set: { activeSession: "" } },
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            res.status(400).send("err while getting session");
+            client.close();
+            return;
+          }
+          res.status(200).send("Logout success");
+          client.close();
+        },
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+router.post("/validate_token", (req, res) => {
   const body = req.body;
-  const hash = crypto.createHash("md5").digest("hex");
-  client.connect(async (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send("err");
-      return;
-    }
-    const collection = client.db("Dollop").collection("users");
-    await collection
-      .find({ _id: body.email })
-      .project({ password: 1 })
-      .toArray((err, result) => {
+  // body has email and token
+  try {
+    client.connect(async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+      const collection = client.db("Dollop").collection("users");
+      collection.find({ _id: body.email }).toArray((err, result) => {
         if (err) {
           console.log(err);
           client.close();
           res.status(400).send("err");
           return;
         }
-        if (
-          crypto.createHash("md5").update(body.password).digest("hex") ==
-          result[0].password
-        ) {
+        if (body.token == result[0].activeSession) {
           console.log("pass");
-          res.status(200).send(hash);
-          collection.updateOne(
-            { _id: body.email },
-            { $set: { activeSession: hash } },
-            (err, result) => {
-              if (err) {
-                console.log(err);
-                res.status(400).send("err while adding session");
-                client.close();
-                return;
-              }
-              res.status(200).send("Login success");
-              client.close();
-            },
-          );
+          res.status(200).send("Active");
         } else {
           console.log(body.password, result[0].password);
-          client.close();
-          res.status(400).send("Invalid credentials");
+          res.status(400).send("Invalid session");
         }
+        client.close();
       });
-  });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
-router.post("/logout", (req, res) => {
-  const body = req.body;
-  client.connect(async (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send("err");
-      return;
-    }
-    const collection = client.db("Dollop").collection("users");
-    collection.updateOne(
-      { _id: body.email },
-      { $set: { activeSession: "" } },
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          res.status(400).send("err while getting session");
-          client.close();
-          return;
-        }
-        res.status(200).send("Logout success");
-        client.close();
-      },
-    );
-  });
+// TBD
+
+router.post("/update", upload.array("uploadedFile", 5), async (req, res) => {
+  // body has obj to be updated
+  try {
+    const body = req.body;
+    const obj = body.obj;
+    client.connect(async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+
+      if (req.file) {
+        var image = req.files[0];
+        var bannerImage = req.files[0];
+
+        var mimetype = image.mimetype;
+        mimetype = mimetype.split("/");
+        var extension = mimetype[1];
+        console.log(image.buffer);
+        const file = bucket.file(`Images/${obj._id}/${obj.name}.${extension}`);
+        await file.save(image.buffer, { contentType: image.mimetype });
+        file.makePublic();
+        const imgLink = file.publicUrl();
+        obj.image = imgLink;
+
+        mimetype = bannerImage.mimetype;
+        mimetype = mimetype.split("/");
+        extension = mimetype[1];
+        console.log(bannerImage.buffer);
+        const bannerImageFile = bucket.file(
+          `Images/${obj._id}/${obj.name}.${extension}`,
+        );
+        await bannerImageFile.save(bannerImage.buffer, {
+          contentType: bannerImage.mimetype,
+        });
+        bannerImageFile.makePublic();
+        const bannerImageLink = bannerImageFile.publicUrl();
+        obj.bannerImage = bannerImageLink;
+      }
+
+      const collection = client.db("Dollop").collection("users");
+      await collection.updateOne({ _id: body.id }, { $set: { obj } });
+      client.close();
+    });
+    res.status(200).send("Success");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
