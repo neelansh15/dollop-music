7a932e1b74b5f707a47bee63d9c148f8b97dab21
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2021-12-01 22:43:03 +0530

    ft: Add CORS support

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,7 +1,9 @@
 const functions = require("firebase-functions");
 const express = require("express");
+var cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const port = 8000;
