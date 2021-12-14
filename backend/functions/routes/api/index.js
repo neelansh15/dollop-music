293e8b64c1a4ab359638610f4b951093b4e821b6
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2021-12-14 12:54:26 +0530

    ft: Add search route
    Search users and music files backend route added

--- backend/functions/routes/api/index.js
+++ backend/functions/routes/api/index.js
@@ -5,6 +5,7 @@ router.use("/profile", require("./profile"));
 router.use("/follow", require("./follow"));
 router.use("/chat", require("./chat"));
 router.use("/music", require("./music"));
+router.use("/search", require("./search"));
 // etc.
 
 module.exports = router;
---
+++ backend/functions/routes/api/search.js
@@ -0,0 +1,51 @@
+var router = require("express").Router();
+const { client } = require("../../db");
+
+router.get("/", async (req, res) => {
+  try {
+    const query = req.body;
+    console.log(query);
+    const response = [];
+    client.connect(async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("err");
+        return;
+      }
+      const musicCollection = client.db("Dollop").collection("music");
+      musicCollection
+        .find({ name: { $regex: `.*${query.name}.*`, $options: "i" } })
+        .toArray((err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(400).send("Error in finding");
+            return;
+          }
+          data.forEach(element => {
+            response.push({ ...element, type: "music" });
+          });
+        });
+      const userCollection = client.db("Dollop").collection("users");
+
+      userCollection
+        .find({ name: { $regex: `.*${query.name}.*`, $options: "i" } })
+        .toArray((err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(400).send("Error in finding");
+            return;
+          }
+          data.forEach(element => {
+            response.push({ ...element, type: "user" });
+          });
+          res.send(response);
+          client.close();
+        });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
