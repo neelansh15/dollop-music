6bc5489080844f53a038da1fb1fdf326e385791b
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2021-12-19 22:53:03 +0530

    ft: Deleting music re-renders profile

--- frontend/pages/profile/index.jsx
+++ frontend/pages/profile/index.jsx
@@ -10,8 +10,8 @@ import PrimaryButton from "../../components/Buttons/Primary";
 import ReactModal from "react-modal";
 
 function profile() {
-  const user = useStore((state) => state.user);
-  const setUser = useStore((state) => state.setUser);
+  const user = useStore(state => state.user);
+  const setUser = useStore(state => state.setUser);
 
   const [musicList, setMusicList] = useState([]);
 
@@ -19,12 +19,38 @@ function profile() {
 
   ReactModal.setAppElement("#__next");
 
+  const fetchData = async () => {
+    if (!user) return;
+    const { data: userDoc } = await axios.get(
+      "http://localhost:8000/api/profile/" + user["_id"],
+    );
+    const userData = {
+      ...user,
+      ...userDoc,
+    };
+    setUser(userData);
+    if (user.music.length > 0) {
+      const { data: musicListArray, status } = await axios.get(
+        "http://localhost:8000/api/music",
+        {
+          params: {
+            ids: user.music,
+          },
+        },
+      );
+
+      if (status === 200) {
+        setMusicList(musicListArray);
+      }
+    }
+  };
+
   useEffect(async () => {
     if (!user) return;
     console.log(user);
     // Update user doc
     const { data: userDoc } = await axios.get(
-      "http://localhost:8000/api/profile/" + user["_id"]
+      "http://localhost:8000/api/profile/" + user["_id"],
     );
     const userData = {
       ...user,
@@ -39,7 +65,7 @@ function profile() {
           params: {
             ids: user.music,
           },
-        }
+        },
       );
 
       if (status === 200) {
@@ -62,28 +88,28 @@ function profile() {
         <title>Profile - Dollop Music</title>
       </Head>
       {user ? (
-        <div className="max-w-2xl md:max-w-7xl mx-5 md:mx-auto mt-10">
+        <div className='max-w-2xl md:max-w-7xl mx-5 md:mx-auto mt-10'>
           {/* Header Card */}
-          <div className="p-8 bg-dark-400 flex items-center justify-between rounded-lg shadow shadow-light-300">
-            <div className="flex items-center space-x-5">
+          <div className='p-8 bg-dark-400 flex items-center justify-between rounded-lg shadow shadow-light-300'>
+            <div className='flex items-center space-x-5'>
               <img
                 src={user.image}
-                alt=""
-                className="w-25 h-25 rounded-full object-cover"
+                alt=''
+                className='w-25 h-25 rounded-full object-cover'
               />
               <div>
-                <h1 className="text-3xl font-semibold">{user.name}</h1>
-                <h3 className="text-gray-400">{user.username}</h3>
+                <h1 className='text-3xl font-semibold'>{user.name}</h1>
+                <h3 className='text-gray-400'>{user.username}</h3>
               </div>
             </div>
             <div>
-              <div className="space-x-2">
+              <div className='space-x-2'>
                 <SecondaryButton>
-                  <i className="fa fa-pencil"></i>&nbsp; Edit
+                  <i className='fa fa-pencil'></i>&nbsp; Edit
                 </SecondaryButton>
                 <PrimaryButton>
                   <span onClick={openUploadModal}>
-                    <i className="fa fa-upload"></i>&nbsp; Upload
+                    <i className='fa fa-upload'></i>&nbsp; Upload
                   </span>
                 </PrimaryButton>
                 {/* <PrimaryButton>Follow</PrimaryButton>
@@ -93,51 +119,55 @@ function profile() {
           </div>
           {/* End of Header Card */}
           {/* Grid */}
-          <div className="mt-8 md:(grid grid-cols-3 gap-x-10)">
+          <div className='mt-8 md:(grid grid-cols-3 gap-x-10)'>
             {/* About the user */}
-            <div className="col-span-2">
+            <div className='col-span-2'>
               <Card>
-                <h1 className="text-xl font-semibold mb-5">About</h1>
+                <h1 className='text-xl font-semibold mb-5'>About</h1>
                 <p>{user.about}</p>
               </Card>
             </div>
             {/* User info */}
-            <div className="col-span-1">
+            <div className='col-span-1'>
               <Card>
-                <div className="flex justify-between">
-                  <div className="-space-y-2">
-                    <h1 className="text-sm font-medium mb-5">Followers</h1>
-                    <p className="text-2xl">{user.followers}</p>
+                <div className='flex justify-between'>
+                  <div className='-space-y-2'>
+                    <h1 className='text-sm font-medium mb-5'>Followers</h1>
+                    <p className='text-2xl'>{user.followers}</p>
                   </div>
-                  <div className="-space-y-2">
-                    <h1 className="text-sm font-medium mb-5">Following</h1>
-                    <p className="text-2xl">{user.following}</p>
+                  <div className='-space-y-2'>
+                    <h1 className='text-sm font-medium mb-5'>Following</h1>
+                    <p className='text-2xl'>{user.following}</p>
                   </div>
                 </div>
               </Card>
             </div>
           </div>
           {/* User posts section */}
-          <div className="mt-8">
+          <div className='mt-8'>
             <Card>
-              <h1 className="text-xl font-semibold mb-5">Your Music</h1>
+              <h1 className='text-xl font-semibold mb-5'>Your Music</h1>
               {musicList.length === 0 && <h3>No music yet</h3>}
-              {musicList.map((music) => (
-                <MusicItem music={music} isOwner={true} />
+              {musicList.map(music => (
+                <MusicItem
+                  music={music}
+                  isOwner={true}
+                  onDeleteCallback={fetchData}
+                />
               ))}
             </Card>
           </div>
           {/* End of user posts section */}
         </div>
       ) : (
-        <div className="max-w-2xl md:max-w-7xl mx-5 md:mx-auto mt-10">
-          <h1 className="text-lg">
+        <div className='max-w-2xl md:max-w-7xl mx-5 md:mx-auto mt-10'>
+          <h1 className='text-lg'>
             <span>
-              <i className="fa fa-lock"></i>
+              <i className='fa fa-lock'></i>
             </span>{" "}
             &nbsp;
-            <Link href="/login">
-              <span className="text-blue-500 cursor-pointer hover:underline">
+            <Link href='/login'>
+              <span className='text-blue-500 cursor-pointer hover:underline'>
                 Log in
               </span>
             </Link>{" "}
@@ -151,10 +181,8 @@ function profile() {
         isOpen={uploadModalOpen}
         onRequestClose={closeUploadModal}
         shouldCloseOnOverlayClick={true}
-        className="bg-dark-900 text-white mx-auto mt-20 w-4/5 md:w-3/4"
-      >
-        
-      </ReactModal>
+        className='bg-dark-900 text-white mx-auto mt-20 w-4/5 md:w-3/4'
+      ></ReactModal>
     </div>
   );
 }
