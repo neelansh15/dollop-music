dad3fd404acaa1409d30c7e0bc22f2632226a72f
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2021-12-19 17:58:08 +0530

    ft: delete functionality integrated with backend

--- frontend/components/MusicItem.jsx
+++ frontend/components/MusicItem.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { useStore } from "store";
 
 export default function MusicItem({ music, isOwner = false }) {
-  const user = useStore((state) => state.user);
-  const musicState = useStore((state) => state.music);
-  const setMusic = useStore((state) => state.setMusic);
+  const user = useStore(state => state.user);
+  const musicState = useStore(state => state.music);
+  const setMusic = useStore(state => state.setMusic);
 
   const [claps, setClaps] = useState(music.claps);
   async function clap() {
@@ -14,11 +14,30 @@ export default function MusicItem({ music, isOwner = false }) {
       "http://localhost:8000/api/music/clap",
       {
         id: id,
-      }
+      },
     );
     if (status === 200) {
       setClaps(claps + 1);
     } else console.error("Unable to clap " + data);
+  }
+
+  async function deleteMusic() {
+    if (window.confirm("Are you sure you want to delete", music.name, "?")) {
+      const id = music["_id"];
+      const { data, status } = await axios.delete(
+        "http://localhost:8000/api/music/",
+        {
+          data: {
+            id: id,
+            userId: user._id,
+            token: user.token,
+          },
+        },
+      );
+      if (status === 200) {
+        console.log("Deleted");
+      } else console.error("Unable to delete " + data);
+    }
   }
 
   function playMusic() {
@@ -33,37 +52,40 @@ export default function MusicItem({ music, isOwner = false }) {
   }
   return (
     <div
-      className="flex justify-between items-center mt-1 rounded-lg p-2 hover:(bg-dark-100 cursor-pointer)"
+      className='flex justify-between items-center mt-1 rounded-lg p-2 hover:(bg-dark-100 cursor-pointer)'
       onClick={playMusic}
     >
-      <div className="flex space-x-3 items-center">
+      <div className='flex space-x-3 items-center'>
         <img
           src={music.image}
-          alt=""
-          className="rounded-lg w-15 h-15 object-cover"
+          alt=''
+          className='rounded-lg w-15 h-15 object-cover'
         />
         <div>
-          <h1 className="font-bold">{music.name}</h1>
+          <h1 className='font-bold'>{music.name}</h1>
           {music.artists ? (
-            <p className="text-sm text-gray-300">{music.artists}</p>
+            <p className='text-sm text-gray-300'>{music.artists}</p>
           ) : (
             <p />
           )}
         </div>
       </div>
       {user && (
-        <div className="flex space-x-2">
+        <div className='flex space-x-2'>
           <div
             onClick={clap}
-            className="flex space-x-2 items-center cursor-pointer"
+            className='flex space-x-2 items-center cursor-pointer'
           >
-            <span className="text-xl">👏🏻</span>
+            <span className='text-xl'>👏🏻</span>
             <span>{claps}</span>
           </div>
           {isOwner == true ? (
             <div>
-              <span className="mr-2">|</span>
-              <button className="text-red-500 px-2 focus:(outline-none ring ring-red-600) transition hover:(text-red-600)">
+              <span className='mr-2'>|</span>
+              <button
+                onClick={deleteMusic}
+                className='text-red-500 px-2 focus:(outline-none ring ring-red-600) transition hover:(text-red-600)'
+              >
                 Delete
               </button>
             </div>
