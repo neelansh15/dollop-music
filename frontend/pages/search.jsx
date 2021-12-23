a5439a0df74d2043509a738cb777030d1453279e
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2021-12-23 23:50:18 +0530

    fix: Multiple instances of same user shown in search fix

--- frontend/pages/search.jsx
+++ frontend/pages/search.jsx
@@ -11,6 +11,8 @@ export default function Search() {
   let [searchString, setSearchString] = useState("");
 
   const getResults = async () => {
+    setSearchedMusic([]);
+    setSearchedUser([]);
     let searchResults = await axios({
       url: "http://localhost:8000/api/search",
       params: { name: searchString },
@@ -23,7 +25,7 @@ export default function Search() {
     let tempMusic = [];
     let tempUsers = [];
 
-    searchResults.data.forEach((element) => {
+    searchResults.data.forEach(element => {
       if (element.type == "user") {
         tempUsers.push(element);
       } else if (element.type == "music") {
@@ -38,12 +40,12 @@ export default function Search() {
     // console.log(tempUsers);
   };
 
-  const handleSearchChange = (event) => {
+  const handleSearchChange = event => {
     setSearchString(event.target.value);
   };
 
   return (
-    <div className="px-15 pt-10 w-full">
+    <div className='px-15 pt-10 w-full'>
       <Head>
         {searchString.length > 0 ? (
           <title>{searchString} - Dollop Music</title>
@@ -52,20 +54,20 @@ export default function Search() {
         )}
       </Head>
       {/* Header */}
-      <h1 className="pb-3 text-3xl font-bold">Search</h1>
+      <h1 className='pb-3 text-3xl font-bold'>Search</h1>
 
       {/* Search Bar */}
-      <div className="bg-dark-300 p-2 mb-3 w-full rounded-lg flex justify-between">
-        <i className="fa fa-search ml-3" aria-hidden="true"></i>
+      <div className='bg-dark-300 p-2 mb-3 w-full rounded-lg flex justify-between'>
+        <i className='fa fa-search ml-3' aria-hidden='true'></i>
 
         <input
           value={searchString}
           onChange={handleSearchChange}
-          className="bg-dark-300 w-17/20 rounded-lg"
+          className='bg-dark-300 w-17/20 rounded-lg'
         ></input>
 
         <button
-          className="px-5 bg-dark-300 text-white font-semibold text-md rounded-lg"
+          className='px-5 bg-dark-300 text-white font-semibold text-md rounded-lg'
           onClick={getResults}
         >
           Search
@@ -75,23 +77,23 @@ export default function Search() {
       {/* Results */}
       {searchedUser.length == 0 ? null : (
         <Card>
-          <h1 className="text-xl font-semibold mb-5">User Results</h1>
+          <h1 className='text-xl font-semibold mb-5'>User Results</h1>
 
-          {searchedUser.map((user) => (
+          {searchedUser.map(user => (
             <ArtistItem artist={user} key={user.name} />
           ))}
         </Card>
       )}
 
-      <div className="my-5">
+      <div className='my-5'>
         <p />
       </div>
 
       {searchedMusic.length == 0 ? null : (
         <Card>
-          <h1 className="text-xl font-semibold mb-5">Song Results</h1>
+          <h1 className='text-xl font-semibold mb-5'>Song Results</h1>
 
-          {searchedMusic.map((music) => (
+          {searchedMusic.map(music => (
             <MusicItem music={music} key={music.name} />
           ))}
         </Card>
